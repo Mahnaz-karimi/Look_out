b5061c22ca597e6b932fa223db118027956a2451
mk2020 <mahnaaz85@gmail.com>
2021-07-07 20:59:57 +0200

    blog ændringer

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now) # finde tid som en post bliver gemt
+    date_posted = models.DateTimeField(default=timezone.now)  # finde tid som en post bliver gemt
     author = models.ForeignKey(User, on_delete=models.CASCADE) # import User clas med forenkey post bliver forbundet med user.
     # on_delete gøre at hvis en bruger bliver slettet så vil posten bliver også slettet. den gøres med Cascade med store bogstaver
 
