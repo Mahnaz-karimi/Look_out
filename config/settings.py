7663a2a4c846e81711994569053e74a80b898d25
mk2020 <mahnaaz85@gmail.com>
2021-07-17 02:14:07 +0200

    Debug value til False in settings.py

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ else:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
+    DEBUG = True
     ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
     DATABASES = {}
     if 'DYNO' in os.environ:  # Dette sker kun på Heroku, hvis man er på heroku så skal dette settes op
