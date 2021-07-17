aff72393bdaf9cf2263956bacdc12e25532367bf
mk2020 <mahnaaz85@gmail.com>
2021-07-17 14:29:45 +0200

    add a felt 'title' in blog.models.py

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)  # finde tid som en post bliver gemt
     author = models.ForeignKey(User, on_delete=models.CASCADE) # import User clas med forenkey post bliver forbundet med user.
