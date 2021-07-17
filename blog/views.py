eef47f409516dd8d56f05c6c9ebf9f23b0420ab4
mk2020 <mahnaaz85@gmail.com>
2021-07-17 14:50:50 +0200

    sorter post is changed in views.py

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,6 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'
     context_object_name = 'posts'
-    paginate_by = 5
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
