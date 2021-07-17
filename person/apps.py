3a3aa97376c257159aeaa1790b7792b84b6848e1
mk2020 <mahnaaz85@gmail.com>
2021-07-17 02:10:53 +0200

    app.py is modified for flake8

--- person/apps.py
+++ person/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class PersonConfig(AppConfig):
     name = 'person'
 
-    def ready(self):  # appen vil bruge signal for at oprette en profile når en user er added
+    def ready(self):  #  appen vil bruge signal for at oprette en profile når en user er added
         import person.signals as signal
         print(signal)
