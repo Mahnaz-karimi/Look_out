901aabd280dc45345c556f3d8e95502c7f83c46c
mk2020 <mahnaaz85@gmail.com>
2021-08-12 03:00:31 +0200

    add post is added

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,8 @@ from blog.views import (
     CommentNewPostCreateView,
     CommentDeleteView,
     PhotoUpdateView,
-    PhotoDeleteView
+    PhotoDeleteView,
+    PostCreateView
 )
 
 app_name = 'blog'
@@ -19,12 +20,15 @@ urlpatterns = [
     path('', PhotoListView.as_view(), name='blog-home'),
     path('photo/update/<int:pk>/', PhotoUpdateView.as_view(), name='photo-update'),
     path('photo/delete/<int:pk>/', PhotoDeleteView.as_view(), name='photo-delete'),
-    path('posts/', PostListView.as_view(), name='post-view'),
     path('photo/new/', login_required(PhotoCreateView.as_view()), name='photo-new'),
+
+    path('posts/', PostListView.as_view(), name='post-view'),
+    path('post/new/', PostCreateView.as_view(), name='post-new'),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/comments/<int:id>/', PostCommentsView.as_view(), name='post-comments'),
     path('user/posts/<str:username>', UserPostListView.as_view(), name='user-posts'),
+
     path('post/new/comments/<int:id>/', CommentNewPostCreateView.as_view(), name='comment-new'),
     path('delete/comments/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
