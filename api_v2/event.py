ac5c15e5bedf43a04221620417e9711394f25203
mk2020 <mahnaaz85@gmail.com>
2021-08-22 02:28:20 +0200

    person models added som funktion

--- api_v2/event.py
+++ api_v2/event.py
@@ -1,3 +1,4 @@
+
 subscribers = dict()
 
 
--- person/models.py
+++ person/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from hashlib import blake2b
+
 
 # the 'database' of users
 users = []
@@ -12,6 +14,33 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'  # retuneres brugernavn
+
+
+class User:
+    def __init__(self, name: str, password: str, email: str):
+        self.name = name
+        self.password = blake2b(password.encode('UTF-8')).hexdigest()
+        self.email = email
+        self.plan = "basic"
+        self.reset_code = ""
+
+    def __repr__(self):
+        return f"NAME: {self.name}, EMAIL: {self.email}, PASSWD: {self.password}"
+
+    def reset_password(self, code: str, new_password: str):
+        if code != self.reset_code:
+            raise Exception("Invalid password reset code.")
+        self.password = blake2b(new_password.encode('UTF-8')).hexdigest()
+
+
+# stub methods for actions related to creating a new user
+
+
+def create_user(name: str, password: str, email: str):
+    print(f"DB: creating user database entry for {name} ({email}).")
+    new_user = User(name, password, email)
+    users.append(new_user)
+    return new_user
 
 
 def find_user(email: str):
